73d3e4f7c720ef23db8ac46197226e8d91b80da6
CBLiu <liucb214@ciac.ac.cn>
2022-12-07 21:16:12 +0800

    reformed some file

--- unet/hungarian.py
+++ unet/hungarian.py
@@ -18,7 +18,7 @@ def correspondence(prev, curr):
     solved by the Hungarian algorithm as implemented in the munkres package.
     """
     newcell = np.max(prev) + 1
-    
+
     hu_dict = hungarian_align(prev, curr)
     new = curr.copy()
     for key, val in hu_dict.items():
@@ -26,9 +26,9 @@ def correspondence(prev, curr):
         if val == -1:
             val = newcell
             newcell += 1
-        
-        new[curr==key] = val
-        
+
+        new[curr == key] = val
+
     return new
 
 
@@ -40,42 +40,42 @@ def hungarian_align(m1, m2):
     value is -1.
     """
     dist, ix1, ix2 = cell_distance(m1, m2)
-    
+
     # If dist couldn't be calculated, return dictionary from cells to themselves 
     if dist is None:
         unique_m2 = np.unique(m2)
         return dict(zip(unique_m2, unique_m2))
-    
+
     solver = Munkres()
     indexes = solver.compute(make_square(dist))
-    
+
     # Create dictionary of cell indicies
     d = dict([(ix2.get(i2, -1), ix1.get(i1, -1)) for i1, i2 in indexes])
-    d.pop(-1, None)  
+    d.pop(-1, None)
     return d
 
 
 def cell_to_features(im, c, nsamples=None, time=None):
     """Embeds cell c in image im into feature space"""
-    coord = np.argwhere(im==c)
+    coord = np.argwhere(im == c)
     area = coord.shape[0]
-    
+
     if nsamples is not None:
         samples = np.random.choice(area, min(nsamples, area), replace=False)
-        sampled = coord[samples,:]
+        sampled = coord[samples, :]
     else:
         sampled = coord
-    
+
     com = sampled.mean(axis=0)
-    
+
     return {'cell': c,
             'time': time,
             'sqrtarea': np.sqrt(area),
             'area': area,
             'com_x': com[0],
             'com_y': com[1]}
-    
-    
+
+
 def cell_distance(m1, m2, weight_com=3):
     """
     Gives distance matrix between cells in first and second frame, by embedding
@@ -84,7 +84,7 @@ def cell_distance(m1, m2, weight_com=3):
     make it more important).
     """
     # Modify to compute use more computed features
-    #cols = ['com_x', 'com_y', 'roundness', 'sqrtarea']
+    # cols = ['com_x', 'com_y', 'roundness', 'sqrtarea']
     cols = ['com_x', 'com_y', 'area']
 
     def get_features(m, t):
@@ -93,29 +93,29 @@ def cell_distance(m1, m2, weight_com=3):
             cells.remove(0)
         features = [cell_to_features(m, c, time=t) for c in cells]
         return pd.DataFrame(features), dict(enumerate(cells))
-    
+
     # Create df, rescale
     feat1, ix_to_cell1 = get_features(m1, 1)
     feat2, ix_to_cell2 = get_features(m2, 2)
-    
+
     # Check if one of matrices doesn't contain cells
-    if len(feat1)==0 or len(feat2)==0:
+    if len(feat1) == 0 or len(feat2) == 0:
         return None, None, None
-    
+
     df = pd.concat((feat1, feat2))
     df[cols] = scale(df[cols])
-    
+
     # give more importance to center of mass
     df[['com_x', 'com_y']] = df[['com_x', 'com_y']] * weight_com
 
     # pairwise euclidean dist
     dist = euclidean_distances(
-        df.loc[df['time']==1][cols],
-        df.loc[df['time']==2][cols]
+        df.loc[df['time'] == 1][cols],
+        df.loc[df['time'] == 2][cols]
     )
     return dist, ix_to_cell1, ix_to_cell2
-    
-    
+
+
 def zero_pad(m, shape):
     """Pads matrix with zeros to be of desired shape"""
     out = np.zeros(shape)
@@ -126,12 +126,10 @@ def zero_pad(m, shape):
 
 def make_square(m):
     """Turns matrix into square matrix, as required by Munkres algorithm"""
-    r,c = m.shape
-    if r==c:
+    r, c = m.shape
+    if r == c:
         return m
-    elif r>c:
-        return zero_pad(m, (r,r))
+    elif r > c:
+        return zero_pad(m, (r, r))
     else:
-        return zero_pad(m, (c,c))
-
-    
+        return zero_pad(m, (c, c))
